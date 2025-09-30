87b998a2e5d7a8d88f32e321f697dd0cd8c3a37c
jotaalvim <joaoafonsoalvim@gmail.com>
2025-09-30 17:12:05 +0100

    hash algorithm

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # library-of-babel
 implementation of the library of babel but with more characters allowed
+
+
+ghci -package base64-bytestring -package hashable-1.4.7.0 main.hs
+
+
--- main.hs
+++ main.hs
@@ -1,36 +1,24 @@
-import qualified Data.ByteString.Char8 as B8
 
-import qualified Data.ByteString.Base16 as B16
+import Data.List.Split
+import Data.List
 
-input   = B8.pack "Hello World."
-
-encoded = B16.encode input
+import Data.Hashable (hash)
 
 
--- Hexagon Name : A base-36 string representing the unique identifier of the hexagon.
-type HexagonNumber = String
-
+-- Hash Name : A string representing the unique identifier
+type HashNumber   = Integer
 --    Wall Number : The wall within the hexagon (1–4).
-type   WallNumber = Integer 
+type   WallNumber = Integer
 --   Shelf Number : The shelf on the wall (1–5).
-type  ShelfNumber = Integer 
---  Volume Number : The volume on the shelf (1–32).
-type VolumeNumber = Integer 
+type  ShelfNumber = Integer
+--  Volume Number : The volume on the shelf (1–32). 
+type VolumeNumber = Integer
+type PageNumber   = Integer
 
+data Book = Book HashNumber WallNumber ShelfNumber VolumeNumber PageNumber deriving (Show)
 
-
-data Book = Book HexagonNumber WallNumber ShelfNumber VolumeNumber  deriving (Show)
-    
-
-hexStringcharaterSet = ['0'..'9'] ++ ['a'..'z']
-
-
-charaterSet =  ['a'..'z'] ++ ". ," 
-
-
-getHexAdress (Book h w s v) = h ++"-w" ++ show w ++ "-s" ++ show s ++ "-v" ++ show v
-
-
+-- Total wall per room = 4
+-- Volumes per shelf = 32
 -- Number of Pages: 410
 -- Lines per Page: 40
 -- Characters per Line: 80
@@ -38,4 +26,23 @@ getHexAdress (Book h w s v) = h ++"-w" ++ show w ++ "-s" ++ show s ++ "-v" ++ sh
 -- 410 pages x 40 lines/page x 80 characters/line=1,312,000 characters/book
 -- 410pages x 40lines/page x 80characters/line=1,312,000characters/book
 
-book  = Book "jeb0110jlb" 2 4 16 
+charaterSet =  ['a'..'z'] ++ ". ," 
+
+pages_volume = 410
+pages_shelf  = pages_volume  * 32
+pages_wall   = pages_shelf   * 5
+pages_total  = pages_wall    * 5
+
+str2book :: String -> Book
+str2book s = coords $ abs $ toInteger $ hash s
+    
+coords n = Book n wall shelf volume page
+    where newn = mod n pages_total -- adjust n to fit the total pages
+          (wall  , rem1) = divMod newn pages_wall
+          (shelf , rem2) = divMod rem1 pages_shelf
+          (volume, page) = divMod rem2 pages_volume
+
+
+getHexAdress (Book h w s v p ) = show h ++"-" ++ show w ++ "-" ++ show s ++ "-" ++ show v ++ "-" ++ show p
+
+book  = Book 9876543456789 2 4 16 300
